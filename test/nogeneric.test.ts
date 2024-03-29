251490e3a69cad3cfd227159fa48c5b791a137c5
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-03-29 13:34:23 +0700

    no generic

---
+++ test/nogeneric.test.ts
@@ -0,0 +1,15 @@
+describe("no generic", () => {
+    class Data{
+        value:any;
+        constructor(value:any){
+            this.value=value;
+        }
+    }
+
+    it("should accept all values", async () => {
+        const data=new Data("Fajar");
+        // data.value=100;
+
+        console.info(data.value.toUpperCase());
+    });
+});
